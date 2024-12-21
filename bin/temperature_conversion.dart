34178adc6dfbb217c595315732ef8c7f4bf3fa3a
Mochammad Gamal <mogams1811@gmail.com>
2024-12-21 15:26:39 +0700

    chore: :fire: remove unnecessary files.

--- bin/temperature_conversion.dart
+++
@@ -1,6 +0,0 @@
-import 'package:temperature_conversion/temperature_conversion.dart'
-    as temperature_conversion;
-
-void main(List<String> arguments) {
-  print('Hello world: ${temperature_conversion.calculate()}!');
-}
