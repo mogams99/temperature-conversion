b59bdf0da4ead814456d3fb1d7cc1d1a375f31ea
Mochammad Gamal <mogams1811@gmail.com>
2024-12-21 15:14:20 +0700

    feat: :construction: setup temperature model.

---
+++ lib/models/temperature_model.dart
@@ -0,0 +1,13 @@
+class TemperatureModel {
+  num value;
+
+  TemperatureModel(this.value);
+
+  num toCelcius() => (value - 32) * 5 / 9;
+
+  num toFahrenheit() => (value * 9 / 5) + 32;
+
+  num toKelvinFromCelcius() => toCelcius() + 273.15;
+
+  num toCelciusFromKelvin() => value - 273.15;
+}
