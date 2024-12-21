66e08c3e5bc25658246123ffccd29879ac40c217
Mochammad Gamal <mogams1811@gmail.com>
2024-12-21 13:51:58 +0700

    build: :tada: upload project.

---
+++ bin/temperature_conversion.dart
@@ -0,0 +1,6 @@
+import 'package:temperature_conversion/temperature_conversion.dart'
+    as temperature_conversion;
+
+void main(List<String> arguments) {
+  print('Hello world: ${temperature_conversion.calculate()}!');
+}
